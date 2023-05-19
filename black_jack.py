15bc7b3cf29dab338434e37751fbf305683dd3ee
Spencer Knowlton <knowltonspencer@gmail.com>
2023-05-19 14:32:07 -0600

    drawing and dealing cards

--- black_jack.py
+++ black_jack.py
@@ -1,8 +1,17 @@
+import random
+
 #blackJack Game.
 #Rykers Thoughts on building our game.
     #build a deck
     #have a system to grab random cards.
     #deal cards.
+    #option to draw again
+    #handle Ace as 1 or 11
+    #Bust if over 21
+    #dealer draw until > 16
+    #NOTES:
+        #currently just one deck. 
+        #will not be handlign splits for MVP. (that will come on the next round)
 logo = """
 .------.            _     _            _    _            _    
 |A_  _ |.          | |   | |          | |  (_)          | |   
@@ -42,11 +51,31 @@ for suit in suits:
         # If you want to add images or more info later, you can change the 2nd part of the dict to another dict or a class instance
         card_key = f'{value} of {suit}'
         deck[card_key] = values[value]
-        
-print(deck)
+
+
+def draw_a_card():
+    #Draw a random card
+    card = random.choice(list(deck.keys()))
+    #Get the cards value
+    card_value = deck[card]
+    return [card, card_value]
+
+
+# print(deck)
 
 play = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ").lower() == 'y'
 
 if play:
+    player_card_1 = draw_a_card()
+    player_card_2 = draw_a_card()
+    players_cards = [];
+    players_cards.extend([player_card_1, player_card_2])
+    dealers_card_1 = draw_a_card()
+    dealers_card_2 = draw_a_card()
+    dealers_cards = []
+    dealers_cards.extend([dealers_card_1, dealers_card_2])
     print(logo)
+    print(players_cards)
+    print(dealers_card_1)
+
 
